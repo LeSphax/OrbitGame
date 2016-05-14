337ac07218e3efeeb1edfdc3a464ad6add2d25b7
Sébastien Kerbrat <Sbastien Kerbrat>
2016-05-14 19:23:22 +0200

    Possible to throw planets, gravity and objectives working

---
+++ Assets/Gravity.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    private float mass;
+    private Rigidbody myRigidbody;
+    private const float GRAVITY_CONSTANT = 1;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        if (gameObject.tag == "Planet")
+        {
+            myRigidbody.velocity = Vector3.forward * 10;
+        }
+        mass = myRigidbody.mass;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (GameObject stellarObject in GameObject.FindGameObjectsWithTag("Planet"))
+        {
+            if (stellarObject != gameObject)
+            {
+                //Debug.Log(stellarObject.name);
+                if (Vector3.Distance(transform.position, stellarObject.transform.position) < 1 && gameObject.tag == "Star")
+                {
+                    DestroyPlanet(stellarObject);
+                }
+                stellarObject.GetComponent<Gravity>().ApplyGravityForce(transform.position, mass);
+            }
+
+        }
+
+    }
+
+    public void ApplyGravityForce(Vector3 positionOther, float massOther)
+    {
+
+        if (!myRigidbody.isKinematic)
+        {
+            //Debug.Log(mass * massOther * GRAVITY_CONSTANT + "      " + (mass * massOther * GRAVITY_CONSTANT / Mathf.Pow(Vector3.Distance(transform.position, positionOther), 2)));
+        }
+        Vector3 force = (mass * massOther * GRAVITY_CONSTANT / Mathf.Pow(Vector3.Distance(transform.position, positionOther), 2)) * (positionOther - transform.position);
+        if (!myRigidbody.isKinematic)
+        {
+            //Debug.Log(Vector3.Distance(transform.position, positionOther) + "      " + force.magnitude);
+        }
+        myRigidbody.AddForce(force, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (gameObject.tag == "Star")
+        {
+            DestroyPlanet(other.gameObject);
+        }
+    }
+
+    private void DestroyPlanet(GameObject other)
+    {
+        Instantiate(explosion, other.transform.position, Quaternion.identity);
+        Destroy(other);
+    }
+}
